6918224804afd215349de3568ce98b5aad74a1e3
Alan Bailie <alan.bailie@finance-ni.gov.uk>
2019-05-08 16:41:22 +0100

    Setup dotnet core project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace e2e_demo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
